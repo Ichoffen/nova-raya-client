3696262aefdbef362ebd23c02339f9a9d0d20896
Ichoffen <belkwecreate@gmail.com>
2025-11-25 14:51:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,8 +7,21 @@ function createWindow() {
     height: 780
   });
 
-  if (app.isPackaged) win.loadFile('index.html');
-  else win.loadURL('http://localhost:5173');
+  if (app.isPackaged) {
+    // В собранном приложении открываем скомпилированный фронт
+    const indexPath = path.join(__dirname, "renderer", "index.html");
+    win.loadFile(indexPath);
+  } else {
+    // В режиме разработки — Vite dev server
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
